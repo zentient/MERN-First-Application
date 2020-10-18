e653d6da1ac1706f608832c33341f1a974398f9e
zentient <67522653+zentient@users.noreply.github.com>
2020-10-18 12:47:46 -0400

    Front-End Update #2
    - Installed custom react date picker.
    - Defined and configured the webform for creating a new exercise and updates states.
    - Created the new user form.

--- src/components/CreateExercise.jsx
+++ src/components/CreateExercise.jsx
@@ -1,12 +1,135 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+// Importing the custom DatePicker
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 export default class CreateExercise extends Component {
-    render() {
-        return (
-            <div>
-                CreateExercise
-            </div>
-        )
-    }
-}
+  constructor(props) {
+    super(props);
 
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.onChangeDuration = this.onChangeDuration.bind(this);
+    this.onChangeDate = this.onChangeDate.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      username: "",
+      description: "",
+      duration: 0,
+      date: new Date(),
+      users: [],
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      users: [
+        // temporarily hardcoded users
+        "John Doe",
+        "Jamie Doe",
+        "Jane Doe",
+      ],
+      username: "John Doe",
+    });
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value,
+    });
+  }
+
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value,
+    });
+  }
+
+  onChangeDuration(e) {
+    this.setState({
+      duration: e.target.value,
+    });
+  }
+
+  onChangeDate(date) {
+    this.setState({
+      date
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const newExercise = {
+      username: this.state.username,
+      description: this.state.description,
+      duration: this.state.duration,
+      date: this.state.date,
+    };
+
+    //window.location = "/";
+    console.log(newExercise);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Create New Exercise</h1>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Username: </label>
+            <select
+              required
+              className="form-control"
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+            >
+              {this.state.users.map((user) => {
+                return (
+                  <option key={user} value={user}>
+                    {user}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Description: </label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.description}
+              onChange={this.onChangeDescription}
+            />
+          </div>
+          <div className="form-group">
+            <label>Duration (mins): </label>
+            <input
+              type="number"
+              className="form-control"
+              value={this.state.duration}
+              onChange={this.onChangeDuration}
+            />
+          </div>
+          <div className="form-group">
+            <label>Date: </label>
+            <div>
+              <DatePicker
+                className="form-control"
+                selected={this.state.date}
+                onChange={this.onChangeDate}
+              />
+            </div>
+          </div>
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Create Exercise"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
--- src/components/CreateUser.jsx
+++ src/components/CreateUser.jsx
@@ -1,12 +1,58 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class CreateUser extends Component {
-    render() {
-        return (
-            <div>
-                CreateUser
-            </div>
-        )
-    }
-}
+  constructor(props) {
+    super(props);
 
+    this.onChangeUsername = this.onChangeUsername.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      username: "",
+    };
+  }
+
+  onChangeUsername(e) {
+    this.setState({
+      username: e.target.value,
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const newUser = {
+      username: this.state.username,
+    };
+    console.log(newUser);
+    this.setState({
+      username: "",
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Create New User</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Username: </label>
+            <input
+              type="text"
+              required
+              value={this.state.username}
+              onChange={this.onChangeUsername}
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="submit"
+              value="Create User"
+              className="btn btn-primary"
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
